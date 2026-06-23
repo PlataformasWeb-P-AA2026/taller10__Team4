d2a073049447d26c64f18f8a10ab6e6821c08aea
leosaq <leandrosaquisari7@gmail.com>
2026-06-23 14:21:26 -0500

    cambios

--- ordenamiento/urls.py
+++ ordenamiento/urls.py
@@ -2,28 +2,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
 
-    path('crear/parroquia', views.crear_parroquia,
-         name='crear_parroquia'),
+        path('', views.index, name='index'),
 
-    path('editar/parroquia/<int:id>',
-         views.editar_parroquia,
-         name='editar_parroquia'),
+        path('listar/parroquias',views.listar_parroquias,
+            name='listar_parroquias'),
 
-    path('crear/barrio',
-         views.crear_barrio,
-         name='crear_barrio'),
+        path('listar/barrios',views.listar_barrios,
+            name='listar_barrios'),
 
-    path('editar/barrio/<int:id>',
-         views.editar_barrio,
-         name='editar_barrio'),
+        path('crear/parroquia',views.crear_parroquia,
+            name='crear_parroquia'),
 
-    path('listar/parroquias',
-         views.listar_parroquias,
-         name='listar_parroquias'),
+        path('editar/parroquia/<int:id>',views.editar_parroquia,
+            name='editar_parroquia'),
 
-    path('listar/barrios',
-         views.listar_barrios,
-         name='listar_barrios'),
+        path('crear/barrio',views.crear_barrio,
+            name='crear_barrio'),
+
+        path('editar/barrio/<int:id>',views.editar_barrio,
+            name='editar_barrio'),
 ]
--- ordenamiento/views.py
+++ ordenamiento/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render
 
-from ordenamiento.models import Parroquia, BarrioCiudadela
-from ordenamiento.forms import ParroquiaForm, BarrioCiudadelaForm
+# importar las clases de models.py
+from ordenamiento.models import *
 
+# importar los formularios de forms.py
+from ordenamiento.forms import *
+
+# Create your views here.
 
 def index(request):
+    """
+        Listar los registros del modelo Parroquia y BarrioCiudadela,
+        obtenidos de la base de datos.
+    """
     parroquias = Parroquia.objects.all()
     barrios = BarrioCiudadela.objects.all()
 
@@ -12,37 +23,48 @@ def index(request):
         'parroquias': parroquias,
         'numero_parroquias': len(parroquias),
         'barrios': barrios,
-        'numero_barrios': len(barrios),
+        'numero_barrios': len(barrios)
     }
 
     return render(request, 'index.html', informacion_template)
 
 
 def listar_parroquias(request):
+    """
+        Listar los registros del modelo Parroquia.
+    """
     parroquias = Parroquia.objects.all()
 
     informacion_template = {
         'parroquias': parroquias,
-        'numero_parroquias': len(parroquias),
+        'numero_parroquias': len(parroquias)
     }
 
     return render(request, 'listarParroquias.html', informacion_template)
 
 
 def listar_barrios(request):
+    """
+        Listar los registros del modelo BarrioCiudadela.
+    """
     barrios = BarrioCiudadela.objects.all()
 
     informacion_template = {
         'barrios': barrios,
-        'numero_barrios': len(barrios),
+        'numero_barrios': len(barrios)
     }
 
     return render(request, 'listarBarrios.html', informacion_template)
 
 
 def crear_parroquia(request):
+    """
+        Crear un registro del modelo Parroquia.
+    """
+    print(request)
     if request.method == 'POST':
         formulario = ParroquiaForm(request.POST)
+        print(formulario.errors)
         if formulario.is_valid():
             formulario.save()
             return redirect(index)
@@ -55,10 +77,18 @@ def crear_parroquia(request):
 
 
 def editar_parroquia(request, id):
+    """
+        Editar un registro del modelo Parroquia.
+    """
+    print("---------------")
+    print(request)
+    print("---------------")
+
     parroquia = Parroquia.objects.get(pk=id)
 
     if request.method == 'POST':
         formulario = ParroquiaForm(request.POST, instance=parroquia)
+        print(formulario.errors)
         if formulario.is_valid():
             formulario.save()
             return redirect(index)
@@ -71,13 +101,18 @@ def editar_parroquia(request, id):
 
 
 def crear_barrio(request):
+    """
+        Crear un registro del modelo BarrioCiudadela.
+    """
+    print(request)
     if request.method == 'POST':
-        formulario = BarrioCiudadelaForm(request.POST)
+        formulario = BarrioForm(request.POST)
+        print(formulario.errors)
         if formulario.is_valid():
             formulario.save()
             return redirect(index)
     else:
-        formulario = BarrioCiudadelaForm()
+        formulario = BarrioForm()
 
     diccionario = {'formulario': formulario}
 
@@ -85,15 +120,23 @@ def crear_barrio(request):
 
 
 def editar_barrio(request, id):
+    """
+        Editar un registro del modelo BarrioCiudadela.
+    """
+    print("---------------")
+    print(request)
+    print("---------------")
+
     barrio = BarrioCiudadela.objects.get(pk=id)
 
     if request.method == 'POST':
-        formulario = BarrioCiudadelaForm(request.POST, instance=barrio)
+        formulario = BarrioForm(request.POST, instance=barrio)
+        print(formulario.errors)
         if formulario.is_valid():
             formulario.save()
             return redirect(index)
     else:
-        formulario = BarrioCiudadelaForm(instance=barrio)
+        formulario = BarrioForm(instance=barrio)
 
     diccionario = {'formulario': formulario}
 
